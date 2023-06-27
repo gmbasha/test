846b5b0e368562d0c351c98bfccb6b73d6a0df06
gmbasha <137856945+gmbasha@users.noreply.github.com>
2023-06-27 09:51:05 +0530

    first commit

---
+++ bulit_infunction.py
@@ -0,0 +1,32 @@
+#print syntax:print(data1,data2,......)
+a=10
+b=20
+print(a,b)
+
+
+
+#id syntax: id(variable_name)
+c=20
+d=30
+print(id(a))
+
+
+
+
+#type syntax : type(variable_name/data)
+
+g="hai"
+f=20
+
+print(type(f))
+print(type(g))
+
+
+
+'''length syntax: len(string_name)
+dir   syntax:dir(python_object_name)'''
+
+s="hai hello worald"
+print(len(s))
+print(dir(s))
+
